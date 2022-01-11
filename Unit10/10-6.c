c08a9a3624f1e02fa584f46e1c189389399dfda8
SuJin Park <psjin230@gmail.com>
2022-01-11 21:00:20 +0900

    Revert "10"
    This reverts commit 7baeca43c50674e956cc2aee40d470ba9fa72ce4.

--- Unit10/10-6.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("H%cllo, %s %d %Le\n", 'e', "world!", 8, 4.9e-302L);
-
-	return 0;
-}
--- Unit10/10-8.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("%s %o %lld %Lf\n", "Hello, world!", 076, -2147483647LL, 4.528172L);
-
-	return 0;
-}
