b8209478643d1f92924de648e72c6033c6dabd40
SuJin Park <psjin230@gmail.com>
2022-01-13 15:25:19 +0900

    14

---
+++ Unit14/14-4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int base = 20;
+	int height = 16;
+	int area;
+
+	area = base * height / 2;
+	printf("%d\n", area);
+
+	return 0;
+}
---
+++ Unit14/14-5.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define _USE_MATH_EFINES
+#include <math.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
+int main()
+{
+	float diameter;
+	float radius;
+	float area;
+
+	scanf("%f", &diameter);
+	radius = diameter / 2;
+	area = radius * radius * M_PI;
+
+	printf("%f\n", area);
+	return 0;
+}
