8b09af585c22d7dac303c06664d9661b66e954a0
SuJin Park <psjin230@gmail.com>
2022-01-13 15:20:14 +0900

    13

---
+++ Unit13/13-6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1 = 2;
+	int num2 = 8;
+	int num3; int num4;
+
+	num1++;
+	num3 = --num1;
+
+	--num2;
+	num4 = num2++;
+
+	printf("%d\n", num3);
+	printf("%d\n", num4);
+	return 0;
+}
