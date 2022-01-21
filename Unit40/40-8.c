ef87472905b4a4e5f4bf77f8e506a8e77210d22f
SuJin Park <psjin230@gmail.com>
2022-01-21 19:07:43 +0900

    40

---
+++ Unit40/40-8.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	char a1[31];
+	char b1[31];
+	char c1[31];
+	char d1[31];
+
+	scanf("%s %s %s %s", a1, b1, c1, d1);
+	printf("%s\n%s\n%s\n%s\n", a1, b1, c1, d1);
+
+	return 0;
+}
