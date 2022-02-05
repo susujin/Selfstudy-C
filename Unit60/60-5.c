38380ecd42a2f196855d7623b53d0ac4915d02a4
SuJin Park <psjin230@gmail.com>
2022-02-05 18:10:18 +0900

    60

---
+++ Unit60/60-5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void printName()
+{
+	printf("Beethoven\n");
+}
+
+void printOrdinal()
+{
+	printf("9th\n");
+}
+
+int main()
+{
+	printName();
+	printOrdinal();
+	return 0;
+}
