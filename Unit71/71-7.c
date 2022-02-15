a47fda4adf62e9699639ff55fb1c8aca7d3f858b
SuJin Park <psjin230@gmail.com>
2022-02-15 13:59:34 +0900

    71

---
+++ Unit71/71-7.c
@@ -0,0 +1,31 @@
+/*#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int readData(char* buffer, int offset, int size, FILE* fp)
+{
+	int count;
+
+	if (feof(fp) == 1)
+		return 0;
+
+	fseek(fp, offset, SEEK_SET);
+	count = fread(buffer, sizeof(char), size, fp);
+
+	return count;
+}
+
+int main()
+{
+	char buffer[10] = { 0, };
+	int count;
+
+	FILE* fp = fopen("hello2.txt", "r");
+
+	count = readData(buffer, 9, 3, fp);
+
+	printf("%s, count: %d\n", buffer, count);
+
+	fclose(fp);
+
+	return 0;
+}*/
