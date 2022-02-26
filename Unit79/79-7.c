c658cc66cc7fbe50239b9967d8af381df7bc00bc
SuJin Park <psjin230@gmail.com>
2022-02-26 18:57:25 +0900

    79

---
+++ Unit79/79-7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+static bool output = true;
+
+static bool getOutputConfig()
+{
+	return output;
+}
+
+int main()
+{
+	if (getOutputConfig())
+	{
+		printf("The Little Prince\n");
+	}
+	return 0;
+}
---
+++ Unit79/sub-7.c
@@ -0,0 +1,10 @@
+//79-7
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool output = false;
+bool getOutputConfig()
+{
+	return output;
+}
